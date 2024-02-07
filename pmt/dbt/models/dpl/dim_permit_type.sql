097e2ddeba06ac25ab419faa208416ac49008ccb
Vikas Grover <Vikas.Grover@gov.bc.ca>
2024-02-07 12:19:15 -0800

    creating a placeholder for JR to fill in RRS_Data

--- pmt/dbt/models/dpl/dim_permit_type.sql
+++ pmt/dbt/models/dpl/dim_permit_type.sql
@@ -9,8 +9,25 @@ with ats_data as (
 	from fdw_ods_ats_replication.ats_authorization_instruments aai 
 	where 1=1
 )
+,
+with fta_data as (
+Select distinct 'FTA' as src_sys_code
+ ,pfu.file_type_code AS permit_type_code
+, ftc.description AS permit_type_description
+ from fdw_ods_fta_replication.prov_forest_use pfu
+ left join fdw_ods_fta_replication.file_type_code ftc 
+	ON (ftc.file_type_code = pfu.file_type_code)
+),
+with rrs_data as (
+
+)
 --insert into pmt_dpl.dim_permit_type
 select *
 	from ats_data
+union ALL
+select *
+	from fta_data	
+union ALL
+select *
+	from rrs_data	
 ;
-
