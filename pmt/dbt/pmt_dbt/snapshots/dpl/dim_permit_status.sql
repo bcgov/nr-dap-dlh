4cf383530826b5b098f5a6b3096480b2c7a79dda
Vikas Grover <Vikas.Grover@gov.bc.ca>
2024-02-12 11:16:23 -0800

    completed permit status

--- pmt/dbt/pmt_dbt/snapshots/dpl/dim_permit_status.sql
+++ pmt/dbt/pmt_dbt/snapshots/dpl/dim_permit_status.sql
@@ -12,16 +12,47 @@
     }}
 
 with ats_data as (
-Select 'ATS' as src_sys_code
-,aasc.authorization_status_code as permit_status_code
-,aasc.name as permit_status_description
-,'ATS' || '|'||  coalesce(cast(aasc.authorization_status_code as varchar),'~') as unqid
-from  fdw_ods_ats_replication.ats_authorization_status_codes aasc
-where 1=1
+	Select 'ATS' as src_sys_code
+	,null as permit_status_code
+	,null as permit_status_description
+	,'ATS' || '|'||  coalesce(cast(null as varchar),'~') as unqid
+)
+,fta_data as (
+	Select distinct
+	'FTA' as src_sys_code
+	,hsc.harvest_auth_status_code as permit_status_code
+	,hsc.description as permit_status_description
+	,'FTA' || '|'||  coalesce(cast(hsc.harvest_auth_status_code as varchar),'~') as unqid
+	FROM fdw_ods_fta_replication.harvest_auth_status_code hsc
+)
+,rrs_rp_data as (
+  Select distinct
+  'RRS_RP' as src_sys_code
+  ,rtsc.road_tenure_status_code as permit_status_code
+  ,rtsc.description AS permit_status_description
+  ,'RRS_RP' || '|'||  coalesce(cast(rtsc.road_tenure_status_code as varchar),'~') as unqid
+  FROM fdw_ods_rrs_replication.road_tenure_status_code rtsc
+)
+,rrs_rup_data as (
+  Select distinct
+  'RRS_RUP' as src_sys_code
+  ,null as permit_status_code
+  ,null AS permit_status_description
+  ,'RRS_RUP' || '|'||  coalesce(cast(null as varchar),'~') as unqid
 )
 --insert into pmt_dpl.dim_permit_status
 select *
 	from ats_data
+
+union ALL
+select * from fta_data
+
+union ALL
+select * from rrs_rup_data
+
+union ALL
+select * from rrs_rp_data
 ;
+
 
 {% endsnapshot %}
