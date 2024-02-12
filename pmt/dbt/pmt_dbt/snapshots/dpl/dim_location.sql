f6092d2e6941f483bf07c64910ac49d192c3adfd
Vikas Grover <Vikas.Grover@gov.bc.ca>
2024-02-12 10:10:04 -0800

    Adding rrs and fta

--- pmt/dbt/pmt_dbt/snapshots/dpl/dim_location.sql
+++ pmt/dbt/pmt_dbt/snapshots/dpl/dim_location.sql
@@ -13,19 +13,49 @@
 
 with ats_data as 
 (  select distinct
-  'ATS' as src_sys_code
-  ,proj.location as location_code
-  ,proj.project_name as project_code
+  'ATS' 						as src_sys_code
+  ,proj.location 				as location_code
+  ,proj.project_name 			as project_code
   ,'ATS' || '|'||  coalesce(cast(proj.location as varchar),'~') as unqid
 from fdw_ods_ats_replication.ats_projects proj
 inner join fdw_ods_ats_replication.ats_managing_fcbc_regions amfr
 on (proj.managing_fcbc_region_id = amfr.managing_fcbc_region_id)
 where proj.project_status_code = '1'
 )
---insert into pmt_dpl.dim_location
-select *
-	from ats_data
-;
+, fta_data as 
+(--FTA does not have location as of now
+  Select 
+  'FTA' 						as src_sys_code
+  ,null 						as location_code
+  ,null 						as project_code
+  ,'FTA' || '|'||  coalesce(cast(null as varchar),'~') as unqid
+)
+, RRS_RP_data as 
+(--RRS_RP does not have location as of now
+  Select
+  'RRS_RP' 						as src_sys_code
+  ,null 						as location_code
+  ,null 						as project_code
+  ,'RRS_RP' || '|'||  coalesce(cast(null as varchar),'~') as unqid
+)
+, RRS_RUP_data as 
+(--RRS_RUP does not have location as of now
+  Select
+  'RRS_RUP' 						as src_sys_code
+  ,null 						as location_code
+  ,null 						as project_code
+  ,'RRS_RP' || '|'||  coalesce(cast(null as varchar),'~') as unqid
+)--insert into pmt_dpl.dim_location
+select * from ats_data
+
+union ALL
+select * from fta_data
+
+union ALL
+select * from rrs_rup_data
+
+union ALL
+select * from rrs_rp_data
 ;
 
 {% endsnapshot %}
--- pmt/dbt/pmt_dbt/snapshots/dpl/dim_source_system.sql
+++ pmt/dbt/pmt_dbt/snapshots/dpl/dim_source_system.sql
@@ -1,19 +1,35 @@
-{{ config(materialized='table',
-		  unique_key = ['src_sys_code']
-		) 
-}}
-with ats_data as (
+{% snapshot dim_source_system %}
+
+    {{
+        config(
+          target_schema='pmt_dpl',
+          strategy='check',
+          unique_key='unqid',
+          check_cols=['src_sys_description'],
+		  invalidate_hard_deletes=True,
+          bind=False,
+        )
+    }}
+
+with corp_data as (
 	select 'ATS' as src_sys_code,
-	'Authorization Tracking System' as src_sys_description
+	'Authorization Tracking System' as src_sys_description,
+	'ATS' as unqid
 	union ALL
 	select 'FTA' as src_sys_code,
-	'	Forest Tenure Administration' as src_sys_description
+	'Forest Tenure Administration' as src_sys_description,
+	'FTA' as unqid
 	union ALL
-	select 'RRS' as src_sys_code,
-	'Resource Roads System' as src_sys_description
+	select 'RRS_RP' as src_sys_code,
+	'Resource Roads System - Road Permits' as src_sys_description,
+	'RRS_RP' as unqid
+	union ALL
+	select 'RRS_RUP' as src_sys_code,
+	'Resource Roads System - Road Use Permits' as src_sys_description,
+	'RRS_RUP' as unqid
 )
 --insert into pmt_dpl.dim_source_system
-select * from ats_data
+select * from corp_data
 ;
 
 {% endsnapshot %}
