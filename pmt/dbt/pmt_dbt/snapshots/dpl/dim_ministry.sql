e630583429191829c2851430a41a7a1983ebe552
Vikas Grover <Vikas.Grover@gov.bc.ca>
2024-02-13 14:59:16 -0800

    updating alias

--- pmt/dbt/pmt_dbt/snapshots/dpl/dim_ministry.sql
+++ pmt/dbt/pmt_dbt/snapshots/dpl/dim_ministry.sql
@@ -11,7 +11,7 @@
         )
     }}
 
-with ats_data as (
+with corp_data as (
   	select distinct 'CORP' as src_sys_code
     ,CASE
                       WHEN ats.authorization_instrument_id = ANY (ARRAY[961, 120, 1141, 76, 741, 742, 581, 742, 582, 744, 743, 583, 1061]) THEN 'EMLI'
@@ -24,18 +24,24 @@ with ats_data as (
                       WHEN ats.authorization_instrument_id = ANY (ARRAY[961, 120, 1141, 76, 741, 742, 581, 742, 582, 744, 743, 583, 1061]) THEN 'Ministry of Energy, Mines and Low Carbon Innovation'
                       WHEN ats.authorization_instrument_id = 27 THEN 'Ministry of Forests'
                       WHEN ats.authorization_instrument_id = ANY (ARRAY[2, 20, 682, 19, 1421, 1441]) THEN 'Ministry of Agriculture'
-                      WHEN ats.authorization_instrument_id = ANY (ARRAY[1282, 1281, 1343, 1344, 1361, 1301, 1302, 1303, 1304, 1283, 60, 1341, 1305, 1306, 1342, 21, 107]) THEN 'Minitry of Environment'
+                      WHEN ats.authorization_instrument_id = ANY (ARRAY[1282, 1281, 1343, 1344, 1361, 1301, 1302, 1303, 1304, 1283, 60, 1341, 1305, 1306, 1342, 21, 107]) THEN 'Ministry of Environment'
                       ELSE NULL
                   END AS ministry_description 
 				  
-  ,'CORP' || '|'||  coalesce(cast(ministry_code as varchar),'~') as unqid
+  ,'CORP' || '|'||  coalesce(cast(CASE
+                      WHEN ats.authorization_instrument_id = ANY (ARRAY[961, 120, 1141, 76, 741, 742, 581, 742, 582, 744, 743, 583, 1061]) THEN 'EMLI'
+                      WHEN ats.authorization_instrument_id = 27 THEN 'FOR'
+                      WHEN ats.authorization_instrument_id = ANY (ARRAY[2, 20, 682, 19, 1421, 1441]) THEN 'AGR'
+                      WHEN ats.authorization_instrument_id = ANY (ARRAY[1282, 1281, 1343, 1344, 1361, 1301, 1302, 1303, 1304, 1283, 60, 1341, 1305, 1306, 1342, 21, 107]) THEN 'ENV'
+                      ELSE NULL
+                  END as varchar),'~') as unqid
   from fdw_ods_ats_replication.ats_authorizations ats
   LEFT JOIN fdw_ods_ats_replication.ats_authorization_status_codes aasc
     ON(ats.authorization_status_code = aasc.authorization_status_code)
   LEFT JOIN fdw_ods_ats_replication.ats_authorization_instruments aai
     ON(ats.authorization_instrument_id = aai.authorization_instrument_id)
   where 1=1
-  AND aasc.authorization_status_code <> '2'
+  AND aasc.authorization_status_code <> '1'
   AND (ats.authorization_instrument_id = 
   ANY (ARRAY[961, 120, 1141, 76, 741, 742, 581, 742, 582, 744, 743, 583, 1061, 69, 542, 1481, 1121, 1124, 
   1125, 1122, 421, 68, 541, 70, 1, 71, 124, 181, 73, 701, 702, 33, 704, 503, 82, 482, 710, 709, 1241, 481, 483, 502, 716, 708, 80, 39, 686, 685, 132, 84, 711, 684, 41, 110, 29, 
@@ -45,7 +51,7 @@ with ats_data as (
   ))
   )
 --insert into pmt_dpl.dim_ministry
-select * from ats_data
+select * from corp_data
 where ministry_code is not null
 ;
 
