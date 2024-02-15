e65f4c2aa5c19d3954bb220d60c23f9c7bc748a1
vishreddy01 <vish.reddy@gov.bc.ca>
2024-02-15 13:01:05 -0800

    changes to dim location and fact permits

--- pmt/dbt/pmt_dbt/snapshots/dpl/dim_location.sql
+++ pmt/dbt/pmt_dbt/snapshots/dpl/dim_location.sql
@@ -5,7 +5,7 @@
           target_schema='pmt_dpl',
           strategy='check',
           unique_key='unqid',
-          check_cols='all',
+          check_cols=['location_code'],
 		  invalidate_hard_deletes=True,
           bind=False,
         )
@@ -28,21 +28,21 @@ where proj.project_status_code = '1'
   ,null 						as location_code
   ,'FTA' || '|'||  coalesce(cast(null as varchar),'~') as unqid
 )
-, RRS_RP_data as 
+, rrs_rp_data as 
 (--RRS_RP does not have location as of now
   Select
   'RRS_RP' 						as src_sys_code
   ,null 						as location_code
   ,'RRS_RP' || '|'||  coalesce(cast(null as varchar),'~') as unqid
 )
-, RRS_RUP_data as 
+, rrs_rup_data as 
 (--RRS_RUP does not have location as of now
   Select
   'RRS_RUP' 						as src_sys_code
   ,null 						as location_code
-  ,'RRS_RP' || '|'||  coalesce(cast(null as varchar),'~') as unqid
+  ,'RRS_RUP' || '|'||  coalesce(cast(null as varchar),'~') as unqid
 )--insert into pmt_dpl.dim_location
-select * from ats_data
+select *  from ats_data
 
 union ALL
 select * from fta_data
